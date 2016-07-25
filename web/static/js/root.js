f8cc024030037e198855a02851191ff0c888fed6
Elijah Kim <kimelijah90@gmail.com>
2016-07-25 11:31:36 -0400

    redux react-redux and router

--- web/static/js/root.js
+++ web/static/js/root.js
@@ -1,13 +1,40 @@
 import React, { Component } from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, browserHistory } from "react-router";
+import { createStore, combineReducers } from "redux";
+import { Provider } from "react-redux";
+import { syncHistoryWithStore, routerReducer } from "react-router-redux";
+
+const reducer = function(initialState = {}, state) {
+  return state;
+}
+
+const store = createStore(combineReducers({
+  reducer: reducer,
+  routing: routerReducer,
+}));
+
+const history = syncHistoryWithStore(browserHistory, store)
 
 class Root extends Component {
   render() {
     return (
+      <Provider store={store}>
+        <Router history={history}>
+          <Route path="/" component={Home} />
+        </Router>
+      </Provider>
+    );
+  }
+}
+
+class Home extends Component {
+  render() {
+    return (
       <div>
-        <p>Hello world</p>
+        <p>Hello World</p>
       </div>
-    )
+    );
   }
 }
 
